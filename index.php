613989d28520ebc96cf78bbd8c77dd37898df29c
mpumzee <mpumzcode@gmail.com>
2021-11-04 15:57:51 +0200

    first commit hello world

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\App();
+
+$app->get('/hello[/{name}]', function ($request,$response,$args) {
+
+  $response->write("Hello, " . $args['name'] . ".");
+
+  return $response;
+
+});
+
+$app->run();
